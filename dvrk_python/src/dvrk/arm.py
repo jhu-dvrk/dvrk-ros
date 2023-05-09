8559e583a70b7c63227da5df2cfff9a9fb504da0
Anton Deguet <anton.deguet@jhu.edu>
2023-05-09 11:35:57 -0400

    arm.py: added `hold`

--- dvrk_python/src/dvrk/arm.py
+++ dvrk_python/src/dvrk/arm.py
@@ -1,7 +1,7 @@
 #  Author(s):  Anton Deguet
 #  Created on: 2016-05
 
-# (C) Copyright 2016-2020 Johns Hopkins University (JHU), All Rights Reserved.
+# (C) Copyright 2016-2023 Johns Hopkins University (JHU), All Rights Reserved.
 
 # --- begin cisst license - do not edit ---
 
@@ -77,6 +77,7 @@ class arm(object):
         self.__crtk_utils.add_measured_js()
         self.__crtk_utils.add_measured_cp()
         self.__crtk_utils.add_measured_cv()
+        self.__crtk_utils.add_hold()
         self.__crtk_utils.add_servo_jp()
         self.__crtk_utils.add_servo_jr()
         self.__crtk_utils.add_servo_cp()
