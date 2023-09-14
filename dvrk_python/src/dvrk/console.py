2ebb059cf2829217579a6e3bc24bac330bf9d06b
Anton Deguet <anton.deguet@jhu.edu>
2023-09-14 11:48:16 -0400

    console.py: minor fixes for crtk.ral

--- dvrk_python/src/dvrk/console.py
+++ dvrk_python/src/dvrk/console.py
@@ -1,7 +1,7 @@
 #  Author(s):  Anton Deguet
 #  Created on: 2016-05
 
-# (C) Copyright 2016-2021 Johns Hopkins University (JHU), All Rights Reserved.
+# (C) Copyright 2016-2023 Johns Hopkins University (JHU), All Rights Reserved.
 
 # --- begin cisst license - do not edit ---
 
@@ -18,38 +18,38 @@ class console(object):
     """
 
     # initialize the console
-    def __init__(self, console_namespace = ''):
+    def __init__(self, ral, console_namespace = 'console'):
         # base class constructor in separate method so it can be called in derived classes
-        self.__init_console(console_namespace)
+        self.__init_console(ral, console_namespace)
 
     def __init_console(self, ral, console_name):
         """Default is console name is 'console' and it would be necessary to change it only if
         you have multiple dVRK consoles"""
         # data members, event based
-        self._ral = ral.create_child(console_name)
-        self._teleop_scale = 0.0
+        self.__ral = ral.create_child(console_name)
+        self.__teleop_scale = 0.0
 
         # publishers
-        self._power_off_pub = self._ral.publisher('/power_off',
-                                                  Empty,
-                                                  latch = True, queue_size = 1)
-        self._power_on_pub = self._ral.publisher('/power_on',
-                                                 Empty,
-                                                 latch = True, queue_size = 1)
-        self._home_pub = self._ral.publisher('/home',
-                                             Empty,
-                                             latch = True, queue_size = 1)
-        self._teleop_enable_pub = self._ral.publisher('/teleop/enable',
-                                                      Bool,
-                                                      latch = True, queue_size = 1)
-        self._teleop_set_scale_pub = self._ral.publisher('/teleop/set_scale',
-                                                         Float64,
-                                                         latch = True, queue_size = 1)
+        self.__power_off_pub = self.__ral.publisher('/power_off',
+                                                    Empty,
+                                                    latch = True, queue_size = 1)
+        self.__power_on_pub = self.__ral.publisher('/power_on',
+                                                   Empty,
+                                                   latch = True, queue_size = 1)
+        self.__home_pub = self.__ral.publisher('/home',
+                                               Empty,
+                                               latch = True, queue_size = 1)
+        self.__teleop_enable_pub = self.__ral.publisher('/teleop/enable',
+                                                        Bool,
+                                                        latch = True, queue_size = 1)
+        self.__teleop_set_scale_pub = self.__ral.publisher('/teleop/set_scale',
+                                                           Float64,
+                                                           latch = True, queue_size = 1)
 
         # subscribers
-        self._teleop_scale_sub = self._ral.subscriber('/teleop/scale',
-                                                      Float64,
-                                                      self.__teleop_scale_cb)
+        self.__teleop_scale_sub = self.__ral.subscriber('/teleop/scale',
+                                                        Float64,
+                                                        self.__teleop_scale_cb)
 
     def __teleop_scale_cb(self, data):
         """Callback for teleop scale.
