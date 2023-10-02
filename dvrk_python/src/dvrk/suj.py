a8d973760b5182a4c50940477e80f6edb7e7dbe5
Anton Deguet <anton.deguet@jhu.edu>
2023-10-02 13:01:22 -0400

    dvrk.suj.py: add wrappers for all arms automatically

--- dvrk_python/src/dvrk/suj.py
+++ dvrk_python/src/dvrk/suj.py
@@ -1,7 +1,7 @@
 #  Author(s):  Anton Deguet
 #  Created on: 2016-05
 
-# (C) Copyright 2016-2021 Johns Hopkins University (JHU), All Rights Reserved.
+# (C) Copyright 2016-2023 Johns Hopkins University (JHU), All Rights Reserved.
 
 # --- begin cisst license - do not edit ---
 
@@ -17,28 +17,27 @@ class suj(object):
     """Simple arm API wrapping around ROS messages
     """
 
-    # local kinematics
-    class __Local:
+    class __Arm:
+
+        # local kinematics
+        class __Local:
+            def __init__(self, ral, expected_interval):
+                self.__crtk_utils = crtk.utils(self, ral, expected_interval)
+                self.__crtk_utils.add_measured_cp()
+
         def __init__(self, ral, expected_interval):
             self.__crtk_utils = crtk.utils(self, ral, expected_interval)
+            self.__crtk_utils.add_operating_state()
+            self.__crtk_utils.add_measured_js()
             self.__crtk_utils.add_measured_cp()
+            self.__crtk_utils.add_move_jp() # for simulated SUJs only
+            self.local = self.__Local(ral.create_child('/local'), expected_interval)
 
-    # initialize the arm
-    def __init__(self, ral, arm_name, expected_interval = 1.0):
-        """Constructor.  This initializes a few data members.It
-        requires an arm name, this will be used to find the ROS
-        topics for the arm being controlled.  For example if the
-        user wants `PSM1`, the ROS topics will be from the namespace
-        `SUJ/PSM1`"""
-        self.suj_ral = ral.create_child('SUJ').create_child(arm_name)
-
-        # crtk features
-        self.__crtk_utils = crtk.utils(self, self.suj_ral, expected_interval)
-
-        # add crtk features that we need and are supported by the dVRK
-        self.__crtk_utils.add_operating_state()
-        self.__crtk_utils.add_measured_js()
-        self.__crtk_utils.add_measured_cp()
-        self.__crtk_utils.add_move_jp()
-
-        self.local = self.__Local(self.suj_ral.create_child('/local'), expected_interval)
+            # initialize the arm
+    def __init__(self, ral, expected_interval = 1.0):
+        """Constructor.  This initializes a few data members and creates
+        instances of classes for each SUJ arm."""
+        ral = ral.create_child('SUJ')
+        self.__crtk_utils = crtk.utils(self, ral, expected_interval)
+        for arm in ('ECM', 'PSM1', 'PSM2', 'PSM3'):
+            setattr(self, arm, self.__Arm(ral.create_child(arm), expected_interval))
