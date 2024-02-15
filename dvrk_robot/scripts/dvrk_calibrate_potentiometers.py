2699d322c29429f40811a7a49f39ac6335df8291
Anton Deguet <anton.deguet@jhu.edu>
2024-02-15 17:07:13 -0500

    Removed packages moved to sawIntuitiveResearchKit

--- dvrk_robot/scripts/dvrk_calibrate_potentiometers.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-import os
-
-print('This program has been moved to the dvrk_python package.\nYou should use: rosrun dvrk_python {}'.format(os.path.basename(sys.argv[0])))
